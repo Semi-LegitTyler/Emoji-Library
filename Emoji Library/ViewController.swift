1138447e7eb8a08505b87bccee794e9c1f52c5b7
Tyler Lang <semilegitstudios@gmail.com>
2017-04-07 10:41:08 -0400

    Added Swift for tableView. Setup for datasource and delegate.

--- Emoji Library/ViewController.swift
+++ Emoji Library/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var emojiTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        emojiTable.dataSource = self
+        emojiTable.delegate = self
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
